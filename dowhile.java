f4dd872f18126a208ddb282908def544d144d827
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-12-14 18:38:02 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+class dowhile{
+    public static void main(String[] args)
+    {
+        int c = 0;
+
+        do {
+            System.out.println("Print Statement");
+            c++;
+        }
+
+        while (c < 0);
+    }
+}
