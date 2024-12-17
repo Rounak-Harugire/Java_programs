45dd75c649f8ce1fccbbbc67c4f812bf53d53f47
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-12-17 19:31:05 +0530

    Add files via upload

---
+++ returnkey.java
@@ -0,0 +1,12 @@
+class returnkey {
+    double RR(double a, double b) {
+        double sum = 0;
+        sum = (a + b) / 2.0;
+      
+        return sum;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(new GFG().RR(5.5, 6.5));
+    }
+}
