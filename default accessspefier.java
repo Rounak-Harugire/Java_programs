8d31070f326cea8548118047a4edb84d26aff9ae
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-12-22 21:00:14 +0530

    Add files via upload

---
+++ default accessspefier.java
@@ -0,0 +1,11 @@
+package p2; 
+import p1.*;
+    
+class DefaultNew { 
+    public static void main(String args[]) { 
+      
+        Default o = new Default(); 
+
+        o.display(); 
+    } 
+}
