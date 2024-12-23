bd7aa168beb7f1b8d9ad3c46367db2302b3eb8c0
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-12-23 20:56:13 +0530

    Add files via upload

---
+++ private access modifier.java
@@ -0,0 +1,16 @@
+package p1;
+
+class A {
+    private void display() {
+        System.out.println("rounak harugire");
+    }
+}
+
+class B {
+    public static void main(String args[]) {
+        A obj = new A();
+      
+       
+        obj.display();
+    }
+}
