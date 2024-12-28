4f7ad23216f5bdaa8287fe643541df87f29ee0eb
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2024-12-28 19:35:43 +0530

    Add files via upload

---
+++ Varargs.java
@@ -0,0 +1,21 @@
+class Test1 {
+	
+	static void fun(int... a)
+	{
+		System.out.println("Number of arguments: " + a.length);
+
+		for (int i : a)
+			System.out.print(i + " ");
+		System.out.println();
+	}
+
+	public static void main(String args[])
+	{
+		
+		fun(100); 
+		
+		fun(1, 2, 3, 4); 
+		
+		fun(); 
+	}
+}
