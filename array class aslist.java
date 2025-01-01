40738a685e21f4d4d55e5bdc85083e4a31c1dbd4
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-01-01 21:00:32 +0530

    Add files via upload

---
+++ array class aslist.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class aslist {
+  
+    public static void main(String[] args)
+    {
+        int intArr[] = { 10, 20, 15, 22, 35 };
+
+        System.out.println("Integer Array as List: "+ Arrays.asList(intArr));
+    }
+}
