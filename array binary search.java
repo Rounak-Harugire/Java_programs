66662198ef513444cc283f807972aee2e7b1d686
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-01-02 18:59:54 +0530

    Add files via upload

---
+++ array binary search.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class searchbinary {
+
+    public static void main(String[] args)
+    {
+
+        int intArr[] = { 10, 20, 15, 22, 35 };
+
+        Arrays.sort(intArr);
+
+        int intKey = 22;
+
+        System.out.println(intKey + " found at index = "+ Arrays.binarySearch(intArr, intKey));
+    }
+}
