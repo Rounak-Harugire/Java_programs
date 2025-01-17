5013beeb15e2a4e4b3c232fc80f298225d5e8050
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-01-17 21:57:55 +0530

    Add files via upload

---
+++ Concatenate Two Strings using concat().java
@@ -0,0 +1,10 @@
+class main {
+ 
+    public static void main(String args[]) {
+      
+        String s = "rounak";
+       
+        s = s.concat("harugire");
+        System.out.println(s);
+    }
+}
