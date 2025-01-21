bc79b88f2d8c198ad21530e45771344f41b5e25b
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-01-21 19:08:56 +0530

    Add files via upload

---
+++ NullPointerException in String concat().java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String args[])
+    {
+        String s1 = "Rounak-";
+        String s2 = null;
+
+
+        String s3 = s1.concat(s2);
+
+        System.out.println(s3);
+    }
+}
