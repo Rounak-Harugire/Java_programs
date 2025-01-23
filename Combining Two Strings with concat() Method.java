63615fa33a32fecb5b6f1e5293f22c1056c2fba8
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-01-23 19:41:06 +0530

    Add files via upload

---
+++ Combining Two Strings with concat() Method.java
@@ -0,0 +1,12 @@
+class main {
+
+    public static void main(String args[]) {
+
+        String s1 = "Rounak";
+        String s2 = "Harugire";
+
+        s1 = s1.concat(s2);
+
+        System.out.println(s1);
+    }
+}
