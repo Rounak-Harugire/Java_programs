be771974b8a8094193b8483988328d8d8a5c3bbe
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-01-31 22:34:37 +0530

    Add files via upload

---
+++ CharArrayToString.java
@@ -0,0 +1,9 @@
+public class CharArrayToString {
+    public static void main(String[] args) {
+        char[] charArray = {'H', 'e', 'l', 'l', 'o', ' ', 'p', 'r', 'o', 'g','r','a','m','e','rs'};
+        
+        String str = new String(charArray);
+        
+        System.out.println("Converted String: " + str);
+    }
+}
