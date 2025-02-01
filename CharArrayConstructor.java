584a4be6cf9a810bd2012f5811b6069f4f641f3f
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-02-01 22:30:24 +0530

    Add files via upload

---
+++ CharArrayConstructor.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char[] char_arr = {'H', 'e', 'l', 'l', 'o'};
+        String str = new String(char_arr);
+        
+        System.out.println(str);  
+    }
+}
