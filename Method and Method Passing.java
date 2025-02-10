a6e38986e7881451df72f5ac745c6856d4fe23e3
ROUNAK HARUGIRE <150342234+Rounak-Harugire@users.noreply.github.com>
2025-02-10 21:32:39 +0530

    Add files via upload

---
+++ Method and Method Passing.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class Student {
+
+    int id;
+    String name;
+
+    public void printStudent()
+    {
+        System.out.println("Id:" + id);
+        System.out.println("Name:" + name);
+    }
+}
+
+class GFG {
+    public static void main(String[] args)
+    {
+        Student obj = new Student();
+
+        obj.id = 1;
+        obj.name = "ABC";
+
+        obj.printStudent();
+    }
+}
